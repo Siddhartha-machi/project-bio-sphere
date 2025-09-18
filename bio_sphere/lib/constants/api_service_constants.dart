f1db1b43ccd93cd2f23279f458548b3ec27148aa
siddhartha.machi <siddhartha.machi@gmail.com>
2025-09-19 00:10:20 +0530

    Feat add layer level error codes.

--- bio_sphere/lib/constants/api_service_constants.dart
+++ bio_sphere/lib/constants/api_service_constants.dart
@@ -15,6 +15,35 @@ class AppProviders {
   static const bloc = 'bloc';
 }
 
+/// App error codes
+
+class _UIErrorCodes {
+  int get codeErr => 90000;
+
+  const _UIErrorCodes();
+}
+
+class _StateErrorCodes {
+  int get codeErr => 80000;
+
+  const _StateErrorCodes();
+}
+
+class _DataErrorCodes {
+  int get codeErr => 70000;
+
+  const _DataErrorCodes();
+}
+
+class _BackendErrorCodes {
+  int get codeErr => 60000;
+
+  const _BackendErrorCodes();
+}
+
 class AppErrorCodes {
-  static const transformErr = 999;
+  static const ui = _UIErrorCodes();
+  static const data = _DataErrorCodes();
+  static const state = _StateErrorCodes();
+  static const backend = _BackendErrorCodes();
 }
